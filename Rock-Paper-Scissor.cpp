1c82f2061ffcb0cb22273a1d884fa44d6f3097bd
UNDAMAG3D <k.tarazi@yahoo.com>
2026-03-10 18:02:57 +0300

    Add structs for round and game result tracking
    Introduced stRoundInfo to store details of each round and stGameResults to track overall game statistics, including win counts and winner information. These additions lay the groundwork for enhanced game state management.

--- Rock-Paper-Scissor.cpp
+++ Rock-Paper-Scissor.cpp
@@ -7,6 +7,23 @@ using namespace std;
 enum enGameChoice {Rock = 1, Paper = 2, Scissor = 3};
 enum enWinner {Player = 1, Computer = 2, Draw = 3 };
 
+struct stRoundInfo {
+    short roundNumber = 0;
+    enGameChoice player1Choice;
+    enGameChoice computerChoice;
+    enWinner winner;
+    string winnerName;
+};
+
+struct stGameResults {
+    short gameRound = 0;
+    short player1WinTimes = 0;
+    short computerWinTimes = 0;
+    short drawTimes = 0;
+    enWinner gameWinner;
+    string winnerName = "";
+};
+
 
 
 void startGame() {
