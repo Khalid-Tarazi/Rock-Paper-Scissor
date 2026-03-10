abbbd54f4f67d13c2995d8dd759cbfe2c3850d3a
UNDAMAG3D <k.tarazi@yahoo.com>
2026-03-10 18:32:12 +0300

    Add core helper functions for game logic and outcome handling
    Introduced utility functions for random number generation, computer choice selection, round and game winner determination, and mapping enum values to string names. These functions centralize and simplify the core logic of the Rock-Paper-Scissor game, improving code organization and readability.

--- Rock-Paper-Scissor.cpp
+++ Rock-Paper-Scissor.cpp
@@ -24,6 +24,47 @@ struct stGameResults {
     string winnerName = "";
 };
 
+int randomNumber(int from, int to) {
+    return rand() % (to - from + 1) + from;
+}
+
+enGameChoice getComputerChoice() {
+    return (enGameChoice) randomNumber(1, 3);
+}
+
+enWinner whoWonTheRound(stRoundInfo roundInfo) {
+    if (roundInfo.player1Choice == roundInfo.computerChoice) {
+        return enWinner::Draw;
+    }
+
+    switch (roundInfo.player1Choice) {
+    case enGameChoice::Rock:
+        return (roundInfo.computerChoice == enGameChoice::Paper) ? enWinner::Computer : enWinner::Player;
+    case enGameChoice::Paper:
+        return (roundInfo.computerChoice == enGameChoice::Scissor) ? enWinner::Computer : enWinner::Player;
+    case enGameChoice::Scissor:
+        return (roundInfo.computerChoice == enGameChoice::Rock) ? enWinner::Computer : enWinner::Player;
+    }
+}
+
+enWinner whoWonTheGame(short player1WinTimes, short computerWinTimes) {
+    if (player1WinTimes > computerWinTimes) return enWinner::Player;
+    else if (computerWinTimes > player1WinTimes) return enWinner::Computer;
+    else return enWinner::Draw;
+}
+
+string choiceName(enGameChoice choice) {
+    string arrGameChoices[3] = {"Rock", "Paper", "Scissor"};
+    return arrGameChoices[choice - 1];
+}
+
+string winnerName(enWinner winner) {
+    string arrWinnerName[3] = {"Player1", "Computer", "Draw"};
+    return arrWinnerName[winner - 1];
+}
+
+
+
 
 
 void startGame() {
